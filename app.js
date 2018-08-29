cf86ea619b21f9b81855ed2ccbe77652bb44320b
joshuageronimo <joshuag31@hotmail.com>
2018-08-29 14:47:59 -0700

    Adam told me to

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 
+app.get('/', function (req, res) {
+  res.render('home')
+});
+
 app.get('/hello-gif', function (req, res) {
   var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
   res.render('hello-gif', {gifUrl: gifUrl})
-})
+});
 
 app.get('/hello-squirrel', function (req, res) {
     res.send('Hello Squirrel')
@@ -19,7 +23,7 @@ app.listen(3000, function () {
 app.get('/greetings/:name', function (req, res) {
   var name = req.params.name;
   res.render('greetings', {name: name});
-})
+});
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
