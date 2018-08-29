7c6c333ca66b84013fe720e1d293936b87559bc5
joshuageronimo <joshuag31@hotmail.com>
2018-08-29 12:41:36 -0700

    Finish part two of the tutorial

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 // app.js
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 
-app.get('/hello-world', function (req, res) {
-  res.send('Hello World');
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/hello-squirrel', function (req, res) {
+    res.send('Hello Squirrel')
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
