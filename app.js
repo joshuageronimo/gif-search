7423066f53456fc987a90c5c46da5652b9fba776
joshuageronimo <joshuag31@hotmail.com>
2018-08-30 11:57:51 -0700

    Finish part 5 of the tutorial

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
 // app.js
+var giphy = require('giphy-api')();
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+// REQUIRE HTTP MODULE
+var http = require('http');
+// INITIALIZE THE GIPHY-API LIBRARY
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.render('home')
+  console.log(req.query.term)
+  var queryString = req.query.term;
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  var term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, function(response) {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+
+    response.on('end', function() {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      var parsed = JSON.parse(body);
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', {gifs: parsed.data})
+    });
+  });
+})
+
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  });
 });
 
 app.get('/hello-gif', function (req, res) {
@@ -15,10 +58,3 @@ app.get('/hello-gif', function (req, res) {
 app.get('/hello-squirrel', function (req, res) {
     res.send('Hello Squirrel')
 });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
