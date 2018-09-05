fb4cad23ef5cdb5aa6ecdcb56fbfd3d7fa29d36b
joshuageronimo <joshuag31@hotmail.com>
2018-09-05 14:40:29 -0700

    Deleted .gitignore file

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 // app.js
-var giphy = require('giphy-api')();
+
 var express = require('express');
-var app = express();
 var exphbs  = require('express-handlebars');
-// REQUIRE HTTP MODULE
 var http = require('http');
-// INITIALIZE THE GIPHY-API LIBRARY
+var giphy = require('giphy-api')();
+var app = express();
 
 app.use(express.static('public'));
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term;
-  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-  var term = encodeURIComponent(queryString);
-  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+    console.log(req.query.term)
+    var queryString = req.query.term; //ask
+    // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+    var term = encodeURIComponent(queryString);
+    // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
 
-  http.get(url, function(response) {
-    // SET ENCODING OF RESPONSE TO UTF8
-    response.setEncoding('utf8');
-
-    var body = '';
+    http.get(url, function(response) {
+        // SET ENCODING OF RESPONSE TO UTF8
+        response.setEncoding('utf8'); //ask
+        var body = '';
 
     response.on('data', function(d) {
       // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
@@ -44,17 +37,15 @@ app.get('/', function (req, res) {
   });
 })
 
-app.get('/', function (req, res) {
-  giphy.search(req.query.term, function (err, response) {
-    res.render('home', {gifs: response.data})
-  });
-});
-
 app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
+    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+    res.render('hello-gif', {gifUrl: gifUrl})
 });
 
 app.get('/hello-squirrel', function (req, res) {
     res.send('Hello Squirrel')
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
